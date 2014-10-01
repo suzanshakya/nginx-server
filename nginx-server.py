821da593efa21878211c8c8f416b3122b1739b84
Sujan Shakya <suzan.shakya@gmail.com>
2014-10-02 01:10:46 +0545

    used mime.types from /tmp

--- nginx-server.py
+++ nginx-server.py
@@ -5,7 +5,7 @@ Usage: %prog [path:.] [port:8000]
 
 import os
 import sys
-import subprocess
+import shutil
 
 conf_template = """\
 error_log stderr;
@@ -17,7 +17,7 @@ events {
 }
 
 http {
-    %(include_mime)s
+    include mime.types;
     default_type application/octet-stream;
 
     gzip             on;
@@ -39,26 +39,6 @@ http {
 }
 """
 
-def include_mime():
-    # default location when nginx is installed by home brew in Mac OS
-    default_location = "/usr/local/etc/nginx/mime.types"
-    if os.path.exists(default_location):
-        mime_file = default_location
-    else:
-        file_to_locate = "*/nginx/mime.types"
-        proc = subprocess.Popen(["locate", file_to_locate], stdout=subprocess.PIPE)
-        output, error = proc.communicate()
-        files = filter(os.path.exists, output.split("\n"))
-        if files:
-            mime_file = files[0]
-        else:
-            print >>sys.stderr, "We could not locate %r file." % file_to_locate
-            mime_file = None
-    if mime_file:
-        return "include %s;" % mime_file
-    else:
-        return ""
-
 def get_realpath(path):
     return os.path.realpath(os.path.join(os.path.abspath(os.curdir), path))
 
@@ -76,8 +56,9 @@ def main():
     address = "http://localhost:%s" % port
 
     conf = "/tmp/nginx.conf"
+    shutil.copyfile('mime.types', '/tmp/mime.types')
     with open(conf, "w") as f:
-        conf_data = conf_template % dict(root=root, port=port, include_mime=include_mime())
+        conf_data = conf_template % dict(root=root, port=port)
         f.write(conf_data)
     print >>sys.stderr, "%r serving in %r" % (root, address)
     os.execvp("nginx", ["nginx", "-c", conf])
